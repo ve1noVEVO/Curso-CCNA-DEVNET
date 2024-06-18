97642b288022f84622db57df223593feb6eaf5a4
NetAcad DEVASC <changeme@example.com>
2024-06-18 21:17:29 +0000

    datos personales

---
+++ datos_personales.py
@@ -0,0 +1,8 @@
+nombre = input("Ingrese su nombre: ")
+apellido = input("Ingrese su apellido: ")
+edad = input("Ingrese su edad: ")
+
+print("\nDatos ingresados:")
+print(f"Nombre: {nombre}")
+print(f"Apellido: {apellido}")
+print(f"Edad: {edad}")
